faeebb5586bc0792f1ef78cca7492b52680c476c
L2L2L2L2 <pkt@ictr.or.kr>
2026-08-05 10:38:27 +0900

    Update notices.js (공지사항 업데이트)

--- notices.js
+++ notices.js
@@ -2,10 +2,10 @@
 const noticeData = {
     "global": "위치 안내 시범 서비스 중입니다.",
     "stations": {
-        "가정역": "1, 2번 출구 공사 중입니다.<br><img src=\"https://www.ictr.or.kr/images/popupzone/20260731_gyesanyeokcaenopi.png\" class=\"w-full max-h-56 object-contain rounded-xl my-1\">"
+        "가정역": "1, 2번 출구 공사 중입니다."
     },
     "places": {
-        "211ES001": "이 곳에 개별 장소 공지를 띄울 수 있습니다.",
-        "209ES001": "에스컬레이터에서는 걷거나 뛰지 마십시오."
+        "209ES001": "이 곳에 개별 장소 공지를 띄울 수 있습니다.",
+        "211ES001": "<img src=\"https://www.ictr.or.kr/images/popupzone/20260731_gyesanyeokcaenopi.png\" class=\"w-full max-h-56 object-contain rounded-xl my-1\">"
     }
 };
