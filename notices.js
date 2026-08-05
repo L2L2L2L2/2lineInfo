907efa208c4f44dd7fbe71a375e5ced559e7ec72
L2L2L2L2 <pkt@ictr.or.kr>
2026-08-05 10:35:03 +0900

    Update notices.js (공지사항 업데이트)

--- notices.js
+++ notices.js
@@ -2,8 +2,7 @@
 const noticeData = {
     "global": "위치 안내 시범 서비스 중입니다.",
     "stations": {
-        "가정역": "<img src=\"https://www.ictr.or.kr/images/popupzone/20260731_gyesanyeokcaenopi.png\" class=\"w-full max-h-56 object-contain rounded-xl my-1\">",
-        "서해구청역": ""
+        "가정역": "1, 2번 출구 공사 중입니다.<br><img src=\"https://www.ictr.or.kr/images/popupzone/20260731_gyesanyeokcaenopi.png\" class=\"w-full max-h-56 object-contain rounded-xl my-1\">"
     },
     "places": {
         "211ES001": "이 곳에 개별 장소 공지를 띄울 수 있습니다.",
